e2d01c02911119712d43e68416b8bc8569cfd99d
Scott Mansell <phiren@gmail.com>
2012-05-16 00:52:56 +1200

    Initial commit.

---
+++ armdisasm.cc
@@ -0,0 +1,10 @@
+/******************************************************************
+ * <phire> ok, I think I understand c++ template metaprogramming  *
+ * <skid_au> oh                                                   *
+ * <phire> that statement is going to come back and bite me later *
+ ******************************************************************
+ * An attempt to implement a nice sort and easy to uderstand ARM disassembler using templates.
+ * Inspired by: http://www.youtube.com/watch?v=y71lli8MS8s
+ */
+
+
